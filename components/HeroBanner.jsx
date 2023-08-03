138e258ed3e4f281f2e93f96f4a2288cfb039c01
Tomek <tomek.bielicki@gmail.com>
2023-08-03 17:20:44 +0200

    banner styles prototype

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import { imageUrlFor } from "../lib/client";
 
+import { ImgWrapper, Image } from "./HeroBanner.style";
+
 const HeroBanner = ({ heroBanner }) => {
-  //console.log("heroBanner ", heroBanner[0].smallText);
-  //console.log("hero img", imageUrlFor(heroBanner.image.asset));
   return (
     <>
       <div className="beats-solo">{heroBanner.smallText}</div>
-      <h3>{heroBanner.smallText}</h3>
+      <h3>{heroBanner.midText}</h3>
       <h1>{heroBanner.largeText1}</h1>
-      <img src={imageUrlFor(heroBanner.image.asset)} />
+      <ImgWrapper>
+        <Image src={imageUrlFor(heroBanner.image.asset)} />
+      </ImgWrapper>
     </>
   );
 };
---
+++ components/HeroBanner.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ImgWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 100vw;
+  position: absolute;
+  top: 0;
+  z-index: -1;
+`;
+
+export const Image = styled.img`
+  max-width: 1080px;
+`;
