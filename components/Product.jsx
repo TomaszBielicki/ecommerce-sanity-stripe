8db101a7e029b3e53db693a75a40319af71bfad5
Tomek <tomek.bielicki@gmail.com>
2023-08-07 07:20:12 +0200

    product styles

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Link from "next/link";
 import { imageUrlFor } from "../lib/client";
-import { ProductWrapper } from "./Product.style";
+import { ProductWrapper, Image, TextWrapper } from "./Product.style";
 
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <ProductWrapper>
       <Link href={`/product/${slug.current}`}>
-        <img src={imageUrlFor(image && image[0])} width={250} height={250} />
-        <p>{name}</p>
-        <p>{price}</p>
+        <Image src={imageUrlFor(image && image[0])} width={250} height={250} />
+        <TextWrapper>
+          <p>{name}</p>
+          <p>{price} zł</p>
+        </TextWrapper>
       </Link>
     </ProductWrapper>
   );
--- components/Product.style.js
+++ components/Product.style.js
@@ -5,13 +5,27 @@ export const ProductWrapper = styled.div`
   justify-content: space-around;
   align-items: center;
   flex-direction: row;
-  padding-top: 300px;
   border: 1px solid black;
-  width: 100vw;
+  width: 80vw;
+  border-radius: 10px;
+  padding: 10px;
 `;
 
 export const ProductsWrapper = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
   flex-direction: row;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  object-fit: contain;
+  border-radius: 10px;
 `;
