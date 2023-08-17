6819350ce6d6655c3a52070207336473ff219c9b
Tomek <tomek.bielicki@gmail.com>
2023-08-17 20:16:23 +0200

    about us section prototype

---
+++ components/AboutUs/AboutUs.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { AboutUsSectionWrapper, TitleAboutUs } from "./AboutUs.style";
+function AboutUs() {
+  return (
+    <AboutUsSectionWrapper>
+      <TitleAboutUs>Why Us</TitleAboutUs>
+      Kobieta Interesujesz się modą i najnowszymi trendami? Nie przegap kolekcji
+      Intimissimi dla kobiet i daj się zaskoczyć bogactwem bielizny damskiej,
+      odzieży, piżam i wielu innych fantastycznych produktów. Wszystko
+      zaprojektowaliśmy z myślą o zaspokojeniu potrzeb każdej kobiety, w każdym
+      momencie dnia i na każdą okazję. BIELIZNA DAMSKA INTIMISSIMI: ESENCJA
+      KOBIECOŚCI Stwórz niezapomniany zestaw bielizny damskiej, wybierając swoje
+      ulubione modele spośród tysiąca stylowych biustonoszy i majtek marki
+      Intimissimi. Zakochaj się w naszych niezwykłych braletkach, opaskach,
+      trójkątach czy biustonoszach push up, dobierając do nich seksowne figi,
+      bokserki, stringi czy brazyliany. Jesteś fanką odważniejszej bielizny? W
+      takim razie spraw sobie mały prezent, robiąc zakupy w naszej kolekcji
+      zmysłowej bielizny koronkowej. BIELIZNA DAMSKA, ODZIEŻ DOMOWA, PIŻAMY I
+      WIELE INNYCH Elegancka w biurze, a zrelaksowana w domu lub z przyjaciółmi
+      - nie ważne, gdzie i z kim jesteś, na nas zawsze możesz liczyć. Odkryj już
+      teraz naszą niezwykle miękką i wygodną odzież domową, a wieczorami otul
+      się wybranym przez siebie kompletem piżam damskich marki Intimissimi.
+      Pozwól sobie na odrobinę przyjemności, odkrywając naszą kolekcję damską
+      pełną nie tylko kobiecej bielizny damskiej, ale także przewiewnych koszul
+      nocnych, zmysłowych szlafroków czy przyjemnych w dotyku bluz i spodni
+      dresowych. BIELIZNA DAMSKA: WYBIERZ SWÓJ ULUBIONY PRODUKT Wybierz już
+      teraz swój ulubiony model spośród wielu zestawów bielizny damskiej
+      Intimissimi i dopełnij swój look o modne ubrania najwyższej jakości.
+      Dzięki Intimissimi będziesz czuć się niezwykle komfortowo, ponieważ to dla
+      Ciebie przygotowujemy produkty dopracowane w każdym szczególe: od kolorów,
+      tkanin takich jak jedwab, koronka Lycra®, wiskoza, modal, bawełna Supima®,
+      przez rozmaite style ubrań: bluzek, topów, podkoszulek, T-shirtów,
+      kardiganów czy zmysłowych body, które można nosić pod marynarkę z
+      legginsami, spodniami czy spódnicą. ZAKOCHAJ SIĘ W CAŁEJ KOLEKCJI DAMSKIEJ
+      Dzięki naszej szerokiej ofercie bielizny damskiej, piżam i odzieży
+      domowej, klientki Intimissimi mogą bez końca zestawiać ze sobą różne
+      produkty z naszej kolekcji. Odkryj, jak podkreślić swoje kształty dzięki
+      naszym biustonoszom i lingerie. Przedefiniuj swój styl wybierając nasze
+      ubrania, eksperymentuj z ciekawymi połączeniami i codziennie odkrywaj nowe
+      kombinacje. Nie możesz się zdecydować? Zainspiruj się naszymi
+      propozycjami. Wolisz niestandardowe rozwiązania? Sprawdź wszystkie nasze
+      propozycje i odkryj bogaty świat bielizny damskiej Intimissimi! Dopełnij
+      swoją stylizację naszymi ubraniami i akcesoriami oraz podkreśl ją jedynym
+      w swoim rodzaju stylem Intimissimi!
+    </AboutUsSectionWrapper>
+  );
+}
+
+export default AboutUs;
---
+++ components/AboutUs/AboutUs.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AboutUsSectionWrapper = styled.div`
+  min-height: 480px;
+  background-color: #f2f2f2;
+  padding: 30px 10px 30px 10px;
+  text-align: justify;
+`;
+
+export const TitleAboutUs = styled.h1`
+  text-align: center;
+  padding-bottom: 30px;
+`;
