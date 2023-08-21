da965e695040e940588874362f70cccf91f54d3f
Tomek <tomek.bielicki@gmail.com>
2023-08-21 18:42:48 +0200

    May like products prototype

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { client } from "../../lib/client";
 import { FooterBanner, HeroBanner, Product, Header } from "../../components";
+import AboutUs from "../../components/AboutUs/AboutUs";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ products, bannerData }) {
@@ -9,6 +10,17 @@ export default function Home({ products, bannerData }) {
       <Header />
       <HeroBanner heroBanner={bannerData[0]} />
 
+      <div className="maylike-products-wrapper">
+        <h2>You may like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <AboutUs />
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
